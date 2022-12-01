4bac69912347117cf52286572a2d69ca6b524cff
HemantTrellance <99024438+HemantTrellance@users.noreply.github.com>
2022-12-01 15:05:06 +0530

    1 file modified

--- constroctor_function/constroctor_function.js
+++ constroctor_function/constroctor_function.js
@@ -6,3 +6,19 @@ export function Circle(radius) {
     console.log(x + y);
   };
 }
+// this is populor method to create object in developer comunity but both are similor
+// this is ES 6 feature if user call without new using thenit will throw error
+function PersonN(firstName, lastName) {
+  if (!new.target) {
+    throw Error('Cannot be called without the new keyword');
+  }
+
+  this.firstName = firstName;
+  this.lastName = lastName;
+
+  this.getFullName = function () {
+    return this.firstName + ' ' + this.lastName;
+  };
+}
+let personk = PersonN('John', 'Doe');
+console.log(personk);
